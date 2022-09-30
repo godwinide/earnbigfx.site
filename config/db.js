83ad0c82b68e36d6b71d0f50b03507e3177c8031
Godwin Idemudia <godwinide@gmail.com>
2022-09-30 23:12:12 +0100

    new

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { connect } = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await connect(process.env.DB_URI);
+        await connect("mongodb+srv://javascriptcoder101:godwin@cluster0.tppwfl7.mongodb.net/Fxstreamcrpto");
         console.log("MongoDB connected.")
     }
     catch (err) {
